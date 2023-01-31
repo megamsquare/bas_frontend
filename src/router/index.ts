81d447b5fdc7721ca2083e8063cde2bbb7f08b77
Michael <msquaremega@gmail.com>
2023-01-31 18:24:09 +0100

    changes done in auth

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,11 @@ const router = createRouter({
   routes: [
     {
       path: '/',
+      name: 'login',
+      component: () => import('../views/LoginView.vue')
+    },
+    {
+      path: '/home',
       name: 'home',
       component: HomeView
     },
