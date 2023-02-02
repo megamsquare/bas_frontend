e0aeaf8d81eae623a65ce6d79a498d6926a04ea2
Michael <msquaremega@gmail.com>
2023-02-02 22:24:17 +0100

    working on login

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -1,5 +1,31 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const username = ref('')
+const password = ref('')
+
+const loginUser = () => {
+    console.log('Username: ', username)
+    console.log('Password: ', password)
+}
+</script>
+
 <template>
     <div>
-        
+        <form @submit.prevent="loginUser">
+            <div>
+                <label for="user-name">Username</label>
+                <input type="text" id="user-name" v-model="username">
+            </div>
+            <div>
+                <label for="user-password">Password</label>
+                <input type="password" id="user-password" v-model="password">
+            </div>
+            <div>
+                <button>
+                    Login
+                </button>
+            </div>
+        </form>
     </div>
 </template>
