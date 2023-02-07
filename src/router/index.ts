a0ac3fd3aca51b409a0146183a5deab0cc3cb888
Michael <msquaremega@gmail.com>
2023-02-07 23:34:10 +0100

    add template to register and arrange navigation route

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,39 @@ const router = createRouter({
   routes: [
     {
       path: '/',
+      name: 'landing',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/LandingView.vue')
+    },
+    {
+      path: '/login',
       name: 'login',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/LoginView.vue')
+    },
+    {
+      path: '/register',
+      name: 'register',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/LoginView.vue')
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/LoginView.vue')
+    },
+    {
+      path: '/team',
+      name: 'team',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
