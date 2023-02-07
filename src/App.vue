59543c260d1ec0b91e9062523ffc126b8cdccd53
Michael <msquaremega@gmail.com>
2023-02-07 16:01:27 +0100

    add header and footer to app

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+import Header from './components/header_footer/Header.vue';
+import Footer from './components/header_footer/Footer.vue';
 </script>
 
 <template>
-  <header>
-    <div>
-    </div>
-  </header>
+  <Header />
+
   <RouterView />
+
+  <Footer />
 </template>
 
 <style scoped>
