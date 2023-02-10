bac0a5293ccbe56fc8717d9942953d63ab9cdd2a
Michael <msquaremega@gmail.com>
2023-02-10 18:40:29 +0100

    create file for landing welcome page

--- src/components/header_footer/Header.vue
+++ src/components/header_footer/Header.vue
@@ -21,14 +21,19 @@ function getRoute() {
                     <img src="@/assets/logo.svg" alt="Business Helper" width=100 height=100>
                 </div>
 
-                <div>
-                    <RouterLink to="/">Home</RouterLink>
-                    <RouterLink to="/about">About</RouterLink>
-                    <RouterLink to="/team">Team</RouterLink>
-                    <div class="get_started">
+                <div class="get_started">
+
+                    <div class="landing_nav">
+                        <RouterLink to="/">Home</RouterLink>
+                        <RouterLink to="/about">About</RouterLink>
+                        <RouterLink to="/team">Team</RouterLink>
+                    </div>
+    
+                    <div class="start_auth">
                         <RouterLink to="/login">Login</RouterLink>
                         <RouterLink to="/register">Sign Up</RouterLink>
                     </div>
+
                 </div>
 
             </nav>
@@ -37,6 +42,7 @@ function getRoute() {
 </template>
 
 <style scoped>
+
 header {
     position: fixed;
     top: 0;
@@ -44,7 +50,6 @@ header {
     right: 0;
     padding: 8px 0;
 }
-
 
 header nav {
     display: flex;
@@ -54,23 +59,32 @@ header nav {
     margin: 0 auto;
 }
 
-header .scrolling {
+header.scrolling {
+    background-color: var(--color-text);
+    padding: 0;
+}
 
+nav .get_started {
+    display: flex;
+}
+
+nav .get_started .start_auth {
+    padding: 0 50px;
 }
 
 header .logo {
-    font-weight: 800;
-    color: var(--color-text);
-    text-transform: uppercase;
-    font-size: 24px;
+    padding: 0 32px;
 }
 
 nav {
     width: 100%;
-    font-size: 12px;
     text-align: center;
     margin-top: 2rem;
-} 
+}
+
+nav.start_auth {
+    font-size: large;
+}
 
 nav a.router-link-exact-active {
     color: var(--color-text);
@@ -79,9 +93,16 @@ nav a.router-link-exact-active:hover {
     background-color: transparent;
 }
 
-nav a {
+nav .landing_nav a {
     display: inline-block;
-    padding: 0 0.5rem; 
-    /* border-left: 1px solid var(--color-border); */
+    padding: 0 0.5rem;
+    margin-top: 4px;
 }
+
+nav .start_auth a {
+    display: inline-block;
+    padding: 0 1rem;
+    font-size: large;
+}
+
 </style>
