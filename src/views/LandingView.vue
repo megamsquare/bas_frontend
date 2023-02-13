a507a58b61f0744aaa4f425dafad1e00ec1188d5
Michael <msquaremega@gmail.com>
2023-02-13 16:31:17 +0100

    styles for left_side landing done

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -3,11 +3,45 @@ import Landing from '@/components/landing/Landing.vue'
 </script>
 
 <template>
-    <div>
+    <div class="left_side">
         <img src="@/assets/logo.svg" alt="Business Helper" class="business_logo" width="125" height="125" />
+
+        <div class="wrap_txt">
+            <h1>You are welcome</h1>
+        </div>
     </div>
 
     <div>
         <Landing />
     </div>
 </template>
+
+<style scoped>
+.left_side {
+    line-height: 1.5;
+    max-height: 100vh;
+}
+
+.business_logo {
+    display: block;
+    margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+    .left_side {
+        display: flex;
+        place-items: center;
+        padding-right: calc(var(--section-gap) / 2);
+    }
+
+    .business_logo {
+        margin: 0 2rem 0 0;
+    }
+
+    .left_side .wrap_txt {
+        display: flex;
+        place-items: flex-start;
+        flex-wrap: wrap;
+    }
+}
+</style>
