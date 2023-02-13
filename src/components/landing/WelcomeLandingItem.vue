6344c070de47aed9c744ba4194c4401b8290dbd0
Michael <msquaremega@gmail.com>
2023-02-13 17:01:27 +0100

    add styles to welcome landing item

--- src/components/landing/WelcomeLandingItem.vue
+++ src/components/landing/WelcomeLandingItem.vue
@@ -13,4 +13,36 @@
 </template>
 
 <style scoped>
+.item {
+    display: flex;
+    margin-top: 2rem;
+}
+
+.details {
+    flex: 1;
+    margin-left: 1rem;
+}
+
+i {
+    display: flex;
+    place-content: center;
+    place-items: center;
+    width: 32px;
+    height: 32px;
+    color: var(--color-text);
+}
+
+h3 {
+    color: var(--color-heading);
+    font-size: 1.2rem;
+    font-weight: 500;
+    margin-bottom: 0.4rem;
+}
+
+@media (min-width: 1024px) {
+    .item {
+        margin: 0;
+        padding: 0.4rem 0 1rem calc(var(--section-gap) / 2);
+    }
+}
 </style>
