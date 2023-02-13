a9c7ac475149af650ea192271635608028c3d427
Michael <msquaremega@gmail.com>
2023-02-13 19:32:43 +0100

    styling for svg component

--- src/components/landing/WelcomeLanding.vue
+++ src/components/landing/WelcomeLanding.vue
@@ -1,5 +1,19 @@
+<script setup lang="ts">
+import WelcomeLandingItem from './WelcomeLandingItem.vue';
+</script>
+
 <template>
-    <div>
-        
-    </div>
+    <WelcomeLandingItem>
+        <template #icon>
+            <img src="@/assets/home_logo.svg" alt="Home Logo">
+        </template>
+
+        <template #heading>
+            Home
+        </template>
+
+        Vue’s
+    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
+    provides you with all information you need to get started.
+    </WelcomeLandingItem>
 </template>
--- src/components/landing/WelcomeLandingItem.vue
+++ src/components/landing/WelcomeLandingItem.vue
@@ -3,9 +3,9 @@
         <i>
             <slot name="icon"></slot>
         </i>
-        <div>
+        <div class="details">
             <h3>
-                <slot name="details"></slot>
+                <slot name="heading"></slot>
             </h3>
             <slot></slot>
         </div>
@@ -43,6 +43,43 @@ h3 {
     .item {
         margin: 0;
         padding: 0.4rem 0 1rem calc(var(--section-gap) / 2);
+    }
+
+    .item:before {
+        content: ' ';
+        border-left: 1px solid var(--color-border);
+        position: absolute;
+        left: 0;
+        bottom: calc(50% + 25px);
+        height: calc(50% - 25px);
+    }
+
+    .item:after {
+        content: ' ';
+        border-left: 1px solid var(--color-border);
+        position: absolute;
+        left: 0;
+        top: calc(50% + 25px);
+        height: calc(50% - 25px);
+    }
+
+    .item:first-of-type:before {
+        display: none;
+    }
+
+    .item:first-of-type:after {
+        display: none;
+    }
+
+    i {
+        top: calc(50% - 25px);
+        left: -26px;
+        position: absolute;
+        border: 1px solid var(--color-border);
+        background: var(--color-background);
+        border-radius: 8px;
+        width: 50px;
+        height: 50px;
     }
 }
 </style>
