0050e0e06a36f883a4a778caa075687115228165
Michael <msquaremega@gmail.com>
2023-02-13 17:00:40 +0100

    add html to

--- src/components/landing/WelcomeLandingItem.vue
+++ src/components/landing/WelcomeLandingItem.vue
@@ -1,5 +1,16 @@
 <template>
-    <div>
-        
+    <div class="item">
+        <i>
+            <slot name="icon"></slot>
+        </i>
+        <div>
+            <h3>
+                <slot name="details"></slot>
+            </h3>
+            <slot></slot>
+        </div>
     </div>
 </template>
+
+<style scoped>
+</style>
