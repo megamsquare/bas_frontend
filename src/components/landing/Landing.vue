6176f2e88f0f850dceeead96e8c1aea5515a3d03
Michael <msquaremega@gmail.com>
2023-02-15 23:18:06 +0100

    set sections and wrapper to view height size

--- src/components/landing/Landing.vue
+++ src/components/landing/Landing.vue
@@ -3,5 +3,69 @@ import WelcomeLanding from './welcome/WelcomeLanding.vue';
 </script>
 
 <template>
-    <WelcomeLanding />
+    <div class="left_side">
+        <img src="@/assets/logo.svg" alt="Business Helper" class="business_logo" width="125" height="125" />
+
+        <div class="wrap_txt">
+            <div class="greetings">
+                <h1 class="green">You are welcome</h1>
+                <h3></h3>
+            </div>
+        </div>
+    </div>
+
+    <div>
+        <WelcomeLanding />
+    </div>
 </template>
+
+<style scoped>
+
+.left_side {
+    line-height: 1.5;
+    max-height: 100vh;
+}
+
+.business_logo {
+    display: block;
+    margin: 0 auto 2rem;
+}
+
+h1 {
+    font-weight: 500;
+    font-size: 2.6rem;
+    top: -10px;
+}
+
+h3 {
+    font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+    text-align: center;
+}
+
+@media (min-width: 1024px) {
+    .left_side {
+        display: flex;
+        place-items: center;
+        padding-right: calc(var(--section-gap) / 2);
+    }
+
+    .business_logo {
+        margin: 0 2rem 0 0;
+    }
+
+    .left_side .wrap_txt {
+        display: flex;
+        place-items: flex-start;
+        flex-wrap: wrap;
+    }
+
+    .greetings h1,
+    .greetings h3 {
+        text-align: left;
+    }
+}
+</style>
--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -3,22 +3,14 @@ import Landing from '@/components/landing/Landing.vue'
 </script>
 
 <template>
-    <div class="welcome_align">
-        <div class="left_side">
-            <img src="@/assets/logo.svg" alt="Business Helper" class="business_logo" width="125" height="125" />
-    
-            <div class="wrap_txt">
-                <div class="greetings">
-                    <h1 class="green">You are welcome</h1>
-                    <h3></h3>
-                </div>
-            </div>
-        </div>
-    
-        <div>
-            <Landing />
-        </div>
-    </div>
+    <section class="welcome_align">
+        <Landing />
+    </section>
+    <!-- <sectionS class="section-1">
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ab similique ex eaque est sapiente optio rerum incidunt placeat et adipisci, esse facere mollitia ipsum minus ea modi quasi sequi quidem architecto. Solut a quae perferendis maiores ad deserunt, deleniti ipsa.
+        </p>
+    </sectionS> -->
 </template>
 
 <style scoped>
@@ -30,52 +22,13 @@ import Landing from '@/components/landing/Landing.vue'
     font-weight: normal;
 }
 
-.left_side {
-    line-height: 1.5;
-    max-height: 100vh;
-}
+section.welcome_align {
+    height: 100vh;
+    align-items: center;
 
-.business_logo {
-    display: block;
-    margin: 0 auto 2rem;
-}
-
-h1 {
-    font-weight: 500;
-    font-size: 2.6rem;
-    top: -10px;
-}
-
-h3 {
-    font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-    text-align: center;
 }
 
 @media (min-width: 1024px) {
-    .left_side {
-        display: flex;
-        place-items: center;
-        padding-right: calc(var(--section-gap) / 2);
-    }
-
-    .business_logo {
-        margin: 0 2rem 0 0;
-    }
-
-    .left_side .wrap_txt {
-        display: flex;
-        place-items: flex-start;
-        flex-wrap: wrap;
-    }
-
-    .greetings h1,
-    .greetings h3 {
-        text-align: left;
-    }
 
     .welcome_align {
         display: grid;
