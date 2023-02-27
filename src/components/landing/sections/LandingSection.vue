39da40e05450bcd534c63eb7787a426790e174ee
Michael <msquaremega@gmail.com>
2023-02-27 13:58:27 +0100

    added template successfully

--- src/components/landing/sections/LandingSection.vue
+++ src/components/landing/sections/LandingSection.vue
@@ -1,5 +1,20 @@
+<script setup lang="ts">
+import LandingSectionItem from './LandingSectionItem.vue';
+</script>
+
 <template>
-    <div>
-        
-    </div>
+    <LandingSectionItem>
+        <template #left-section>
+            <img src="@/assets/Browsing-online.svg" alt="">
+        </template>
+        <template #right-section>
+            <div>
+                <h1> Topic </h1>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus, enim magni aperiam eos dolore cupiditate consectetur. Hic eveniet nulla iusto dolorum, corporis et esse, deserunt totam veniam illum iure error eos. Ipsum eligendi quasi odit cum deleniti exercitationem, cupiditate rem ea, similique nam iure in distinctio velit quaerat laborum veritatis.
+                </p>
+            </div>
+        </template>
+    </LandingSectionItem>
+    
 </template>
