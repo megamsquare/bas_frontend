4055082f55fa5859ce6315cc9489c378c656a741
Michael <msquaremega@gmail.com>
2023-03-06 20:32:58 +0100

    trying different scroll event in vue

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,24 @@
 import { RouterView } from 'vue-router'
 import Header from './components/header_footer/Header.vue';
 import Footer from './components/header_footer/Footer.vue';
+import { onMounted, onUnmounted, ref } from 'vue';
+
+const content = ref();
+
+const doScroll = (event: any) => {
+  const scrollHeight = event.target.scrollHeight;
+  const scrollTop = event.target.scrollTop;
+  const clientHeight = event.target.clientHeight;
+  console.log(event.target)
+}
+
+onMounted(() => {
+  content.value.addEventListener('scroll', doScroll)
+})
+
+onUnmounted(() => {
+  content.value.removeEventListener(doScroll)
+})
 </script>
 
 <template>
