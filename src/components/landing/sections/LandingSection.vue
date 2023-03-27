87162e5a6e39f44ac252157911d57c94eb66fdd7
Michael <msquaremega@gmail.com>
2023-03-27 17:45:51 +0100

    add changes to images

--- src/components/landing/sections/LandingSection.vue
+++ src/components/landing/sections/LandingSection.vue
@@ -3,21 +3,38 @@ import { onMounted, onUnmounted, ref } from 'vue';
 import LandingSectionItem from './LandingSectionItem.vue';
 import IconSection from '@/components/icons/IconSection.vue';
 
-const height = ref('682.76');
-const width = ref('865.76');
+const heightWindow = ref('682.76');
+const widthWindow = ref('865.76');
+
+const heightMobile= ref('26');
+const widthMobile = ref('30');
+
+const isMobile = ref(false)
+
+function changeScreen() {
+    if (window.innerWidth <= 900) {
+        isMobile.value = true;
+        console.log('This is windows width: ', window.innerWidth);
+    } else {
+        isMobile.value = false;
+        console.log('This is else windows width: ', window.innerWidth);
+    }
+}
 
 onMounted(() => {
-    if (window.innerWidth <= 900) {
-        console.log('max width: ', window.innerWidth)
-        height.value = "26";
-        width.value = "30";
-    }
+    window.addEventListener("resize", changeScreen);
+    // if (window.innerWidth <= 900) {
+    //     console.log('max width: ', window.innerWidth)
+    //     height.value = "26";
+    //     width.value = "30";
+    // }
     // console.log(window.innerWidth);
 }),
 
 onUnmounted(() => {
-    height.value = "682.76";
-    width.value = "865.76";
+    // window.removeEventListener("resize", changeScreen);
+    // height.value = "682.76";
+    // width.value = "865.76";
 });
 
 </script>
@@ -25,23 +42,25 @@ onUnmounted(() => {
 <template>
     <LandingSectionItem>
         <template #left-section>
-            <IconSection :height="height" :width="width" ></IconSection>
+            <!-- <IconSection :height="heightWindow" :width="widthWindow" v-show="!isMobile"></IconSection> -->
+            <IconSection :height="heightMobile" :width="widthMobile" v-show="isMobile"></IconSection>
             <!-- <img src="@/assets/Browsing-online.svg" alt=""> -->
         </template>
         <template #right-section>
             <div class="text-item-right">
                 <h3> Making Your Business Easy Everyday </h3>
                 <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus, enim magni aperiam eos dolore cupiditate consectetur. Hic eveniet nulla iusto dolorum, corporis et esse, deserunt totam veniam illum iure error eos. Ipsum eligendi quasi odit cum deleniti exercitationem, cupiditate rem ea, similique nam iure in distinctio velit quaerat laborum veritatis.
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus, enim magni aperiam eos dolore
+                    cupiditate consectetur. Hic eveniet nulla iusto dolorum, corporis et esse, deserunt totam veniam illum
+                    iure error eos. Ipsum eligendi quasi odit cum deleniti exercitationem, cupiditate rem ea, similique nam
+                    iure in distinctio velit quaerat laborum veritatis.
                 </p>
             </div>
         </template>
     </LandingSectionItem>
-    
 </template>
 
 <style scoped>
-
 img {
     display: block;
     margin: 0 auto 2rem;
@@ -66,11 +85,12 @@ img {
         flex: 0 0 50%;
         max-width: 50%;
     }
+
     .text-item-right {
         display: block;
         padding: 0.4rem 0 1rem calc(var(--section-gap) / 2);
     }
-    
+
 }
 
 @media (max-width: 900) {
@@ -80,10 +100,10 @@ img {
         flex: 0 0 50%;
         max-width: 50%;
     }
+
     .text-item-right {
         display: block;
         padding: 0.4rem 0 0.5rem calc(--section-gap) / 2;
     }
 }
-
 </style>
